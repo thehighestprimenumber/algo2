a1970ef8040bf80b30362c27c274f112ba35f248
thehighestprimenumber <maru.c50@gmail.com>
2018-09-25 13:23:39 -0300

    crea MyTextField que carga el valor default en _Italics_

---
+++ batcheador/src/gui/MyTextField.java
@@ -0,0 +1,39 @@
+package gui;
+
+import java.awt.Font;
+import java.lang.reflect.Field;
+
+import javax.swing.JTextField;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
+
+import core.Ibatcheable;
+
+@SuppressWarnings("serial")
+class MyTextField extends JTextField{
+		MyTextField (Field f, Ibatcheable b){
+			try {
+				setText((String) f.get(b));
+				setFont(getFont().deriveFont(Font.ITALIC));
+				
+			} catch (IllegalArgumentException | IllegalAccessException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			} 
+			setMinimumSize(getPreferredSize());
+			addCaretListener(new CaretListener() {
+				@Override
+				public void caretUpdate(CaretEvent e) {
+					try {
+						f.set(b, getText());
+						setFont(getFont().deriveFont(Font.PLAIN));
+					} catch (IllegalArgumentException | IllegalAccessException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+				}
+			});	
+		}
+		
+}
--- batcheador/src/gui/TextControl.java
+++ batcheador/src/gui/TextControl.java
@@ -1,13 +1,8 @@
 package gui;
 
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.Field;
 
 import javax.swing.JTextField;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
 
 import core.Ibatcheable;
 
@@ -15,38 +10,16 @@ public class TextControl extends BatchControl{
 	JTextField input;
 	public TextControl(Field f, Ibatcheable b) {
 		super(f,b);
-		this.input = new JTextField();
-		try {
-			input.setText((String) f.get(b));
-			input.setFont(input.getFont().deriveFont(Font.ITALIC));
-			
-		} catch (IllegalArgumentException | IllegalAccessException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		} 
-		input.setMinimumSize(input.getPreferredSize());
-		input.addCaretListener(new CaretListener() {
-			@Override
-			public void caretUpdate(CaretEvent e) {
-				try {
-					field.set(b, input.getText());
-					input.setFont(input.getFont().deriveFont(Font.PLAIN));
-				} catch (IllegalArgumentException | IllegalAccessException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-			}
-		});
+		this.input = new MyTextField(f, b);
 		panel.add(input);
 	}
 
-	public JTextField getInput() {
-		return input;
+	public String getInput() {
+		return input.getText();
 	}
 
-	public void setInput(JTextField input) {
-		this.input = input;
+	public void setInput(String input) {
+		this.input.setText(input);
 	}
-
 }
+
